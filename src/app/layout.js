f2cee49918ee2e4093ba9538d16af9c4d67dc0b6
Ten Denis <ten.denis2@wb.ru>
2025-05-27 12:55:43 +0300

    fixed favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -30,8 +30,17 @@ export const metadata = {
   title: 'Safetrans',
   description: 'Logistics company',
   icons: {
-    icon: '/favicon.png',
+    icon: [
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      {
+        url: '/apple-touch-icon.png',
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+      },
+    ],
   },
+  manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({ children }) {
